155291db29857d9bdfba4e8a8c30b56936e324d3
RiyazGhori123 <riyaz.shaik20@st.niituniversity.in>
2023-10-29 21:40:19 +0530

    Added frontend

--- House_Price/House_Price/views.py
+++ House_Price/House_Price/views.py
@@ -1,4 +1,31 @@
 from django.shortcuts import render;
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression,Lasso,Ridge
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.compose import make_column_transformer
+from sklearn.pipeline import make_pipeline
+from sklearn.metrics import r2_score
+import pickle
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+import xgboost as xgb
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.svm import SVR
+from tabulate import tabulate
 
 def home(request):
     return render(request, "home.html")
+
+def predict(request):
+    return render(request, "predict.html")
+
+def result(request):
+
+    return render(request, "result.html")
